d0a8a06531f12453c9bff991714922b137e03bc8
Jason <1290918208@qq.com>
2020-03-25 15:11:53 +0800

    我的钱包余额、我的账户明细

--- src/main/java/com/github/binarywang/demo/wx/miniapp/controller/MyWalletController.java
+++ src/main/java/com/github/binarywang/demo/wx/miniapp/controller/MyWalletController.java
@@ -1,6 +1,7 @@
 package com.github.binarywang.demo.wx.miniapp.controller;
 import com.github.binarywang.demo.wx.miniapp.core.Result;
 import com.github.binarywang.demo.wx.miniapp.core.ResultGenerator;
+import com.github.binarywang.demo.wx.miniapp.model.MyParkingLot;
 import com.github.binarywang.demo.wx.miniapp.model.MyWallet;
 import com.github.binarywang.demo.wx.miniapp.service.MyWalletService;
 import com.github.pagehelper.PageHelper;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Condition;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -22,7 +24,7 @@ public class MyWalletController {
     @Resource
     private MyWalletService myWalletService;
 
-    @PostMapping("/add")
+    /*@PostMapping("/add")
     public Result add(MyWallet myWallet) {
         myWalletService.save(myWallet);
         return ResultGenerator.genSuccessResult();
@@ -44,12 +46,24 @@ public class MyWalletController {
     public Result detail(@RequestParam Integer id) {
         MyWallet myWallet = myWalletService.findById(id);
         return ResultGenerator.genSuccessResult(myWallet);
+    }*/
+
+    @PostMapping("/myaccount")
+    public Result myaccount(@RequestParam(defaultValue = "0") Integer userId,@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        PageHelper.startPage(page, size);
+        Condition condition = new Condition(MyWallet.class);
+        condition.createCriteria().andCondition("user_id = " + userId + "");
+        List<MyWallet> list = myWalletService.findByCondition(condition);
+        final double sum = list.stream().mapToDouble(p -> p.getMyBalance()).sum();
+        return ResultGenerator.genSuccessResult(sum);
     }
 
     @PostMapping("/list")
-    public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+    public Result list(@RequestParam(defaultValue = "0") Integer userId,@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
         PageHelper.startPage(page, size);
-        List<MyWallet> list = myWalletService.findAll();
+        Condition condition = new Condition(MyWallet.class);
+        condition.createCriteria().andCondition("user_id = " + userId + "");
+        List<MyWallet> list = myWalletService.findByCondition(condition);
         PageInfo pageInfo = new PageInfo(list);
         return ResultGenerator.genSuccessResult(pageInfo);
     }
